f1d0c4bbd87e4d11cfe6b080fce4e41ab580796f
coreyarch1234 <harrilal@mit.edu>
2017-10-16 21:45:39 -0700

    set up project with react native camera

--- Mathy/App.js
+++ Mathy/App.js
@@ -1,16 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  NativeModules
 } from 'react-native';
+
+import Camera from 'react-native-camera';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -20,18 +18,59 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+    takePicture() {
+       this.camera.capture()
+         .then((data) => {
+             console.log("DATA START");
+             console.log(data);
+             console.log("DATA END")
+             var path = data.path;
+             console.log("PATH START");
+             console.log(path)
+             console.log("PATH END")
+             console.log("TESTING NEW!!!!")
+             NativeModules.ReadImageData.readImage(path, (image) => {
+                 console.log("THE IMAGE DATA IS: ");
+                 console.log(image);
+                 fetch('https://api.mathpix.com/v3/latex', {
+                      method: 'POST',
+                      headers: {
+                        'app_id': 'corey_harrilal_students_makeschool_com',
+                        'app_key': 'ddd5a182cfbd8d0a170c',
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                      },
+                      body: JSON.stringify({
+                        'url':'data:text/plain;base64,' + image
+                      })
+                    })
+                  .then((response) => response.json())
+                  .then((responseJson) => {
+                      console.log("THE RESPONSE JSON IS: ");
+                      console.log(responseJson)
+                      return responseJson;
+                  })
+                  .catch(err => console.error(err))
+             })
+
+         })
+         .catch(err => console.error(err));
+     }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Camera
+           ref={(cam) => {
+             this.camera = cam;
+           }}
+           style={styles.preview}
+           aspect={Camera.constants.Aspect.fill}>
+           <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+       </Camera>
       </View>
     );
   }
@@ -54,4 +93,19 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  preview: {
+   flex: 1,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+   height: Dimensions.get('window').height,
+   width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
